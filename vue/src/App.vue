8575386a2de3afad06409bf2a2a3b7b94f949585
Armin Hucke <arminhucke@protonmail.com>
2024-10-20 18:47:17 -0300

    feat: add register template

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import MainLayout from "@/components/MainLayout.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
--- vue/src/views/RegisterView.vue
+++ vue/src/views/RegisterView.vue
@@ -10,15 +10,15 @@ import RouterButton from "@/components/RouterButton.vue";
 				<RouterButton to="/" class="w-32">Back</RouterButton>
 			</div>
 			<div class="flex h-screen items-center justify-around">
-				<h1 class="text-5xl font-bold max-w-96 leading-[60px] text-center">Log into the countertop!</h1>
+				<h1 class="text-5xl font-bold max-w-96 leading-[60px] text-center">Creating new account</h1>
 				<div
 					class="flex flex-col rounded-3xl border border-stone-900 bg-stone-400 p-5 text-center w-96"
 				>
 					<h2 class=" pt-3 pb-7 text-2xl font-bold text-white">
-						Log into your account
+						Enter new account details
 					</h2>
 					<div class="flex flex-col ">
-						<div class="flex flex-col py-10">
+						<div class="flex flex-col py-5">
 							<label class="text-white font-semibold p-2" for="username">Username</label>
 							<input
 								class="border border-stone-900 rounded-2xl p-2"
@@ -27,7 +27,16 @@ import RouterButton from "@/components/RouterButton.vue";
 								name="username"
 							/>
 						</div>
-						<div class="flex flex-col py-10">
+                        <div class="flex flex-col py-3">
+							<label class="text-white font-semibold p-2" for="username">Email</label>
+							<input
+								class="border border-stone-900 rounded-2xl p-2"
+								placeholder="email..."
+								type="text"
+								name="email"
+							/>
+						</div>
+						<div class="flex flex-col py-3">
 							<label class="text-white font-semibold p-2" for="password">Password</label>
 							<input
 								class="border border-stone-900 rounded-2xl p-2"
@@ -36,15 +45,20 @@ import RouterButton from "@/components/RouterButton.vue";
 								name="password"
 							/>
 						</div>
+                        <div class="flex flex-col py-3">
+							<label class="text-white font-semibold p-2" for="password">Repeat password</label>
+							<input
+								class="border border-stone-900 rounded-2xl p-2"
+								placeholder="Password..."
+								type="password"
+								name="password"
+							/>
+						</div>
 					</div>
-					<div class="flex justify-center items-center">
-						<RouterButton class="w-32" to="/"
-							>Log in</RouterButton
+					<div class="flex justify-center items-center py-5">
+						<RouterButton class="w-32" to="/login"
+							>Sign up!</RouterButton
 						>
-					</div>
-					<div class="py-5">
-						<p>Don't have an account?</p>
-						<RouterLink to="/register" class=" hover:underline font-medium">You can sign up here!</RouterLink>
 					</div>
 				</div>
 			</div>
